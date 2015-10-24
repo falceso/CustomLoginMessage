02865887216f18750b51c18a6d8a71afd0e817d0
falceso <Nathanfal23@gmail.com>
2015-10-24 11:46:42 -0700

    Second Commit

--- src/me/falceso/customlogin/Main.java
+++ src/me/falceso/customlogin/Main.java
@@ -1,7 +1,9 @@
 package me.falceso.customlogin;
 
+import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Main extends JavaPlugin {
@@ -17,6 +19,14 @@ public class Main extends JavaPlugin {
 	}
 	
 	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+		Player player = (Player) sender;
+		if (cmd.getName().equalsIgnoreCase("customloginmessage")){
+			 
+			player.sendMessage(ChatColor.BLUE + "CustomLoginMessage");
+			player.sendMessage(ChatColor.BLUE + "By falceso");
+			player.sendMessage(ChatColor.BLUE + "Short Desc: This is a plugin to do Custom Message when the player joins the server.");
+
+		}
 		return false;	
 		
 	}
